1f6d809acd6a64f4038dd907f9042f1090732176
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2021-04-10 21:54:02 -0300

    playlists e iframes modificacao no vetor

--- src/pages/Musics.tsx
+++ src/pages/Musics.tsx
@@ -2,17 +2,33 @@ import React from 'react';
 import { useParams } from "react-router-dom";
 import styles from '../styles/components/Musics.module.css'
 import Playlists from './Playlists';
+import musics from '../musics.json';
+import { Link, Route } from 'react-router-dom';
+import albuns from '../albuns.json'
+
 
 interface ParamTypes {
   id: string
 }
 
 function Musics() {
-  const {id} = useParams<ParamTypes>();
+  const { id } = useParams<ParamTypes>();
   console.log(id)
 
+  const playList = albuns.map((album) => {
+    return (
+      <Link to={"/playlists/:id" + album.id}>
+        <div key={album.id}>
+          <iframe src={album.link} width="300" height="380" frameBorder="0" allow="encrypted-media"></iframe>
+        </div>
+      </Link>
+    );
+  });
+
   return (
-    <div></div>
+    <div className='Musics'>
+      { playList}
+    </div >
   );
 }
 
