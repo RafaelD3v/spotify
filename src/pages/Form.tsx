c0f34ed563dde76adc9645129e60564140dee302
RafaelD3v <rafaelcontato66738621+RafaelD3v@users.noreply.github.com>
2021-04-29 22:22:05 -0300

    Init Form

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -2,8 +2,18 @@ import React from 'React';
 import styles from '../styles/components/Form.module.css';
 
 interface IMyComponentProps {
+  // email: string
+  // password: string,
+  // day: string,
+  // month: string,
+  // year: string,
+  // gender: string
+}
+
+interface IMyComponentState {
   email: string
   password: string,
+  displayname: string,
   day: string,
   month: string,
   year: string,
@@ -11,7 +21,7 @@ interface IMyComponentProps {
 }
 
 export default class Form extends React.Component<IMyComponentProps, IMyComponentState> {
-  constructor(props: object) {
+  constructor(props: IMyComponentProps) {
     super(props);
 
     this.state = {
@@ -27,7 +37,7 @@ export default class Form extends React.Component<IMyComponentProps, IMyComponen
   }
 
   render() {
-    const change = (evento) => {
+    const change = (evento: any) => {
       const { name, value } = evento.target;
       this.setState({ [name]: value });
     }
@@ -95,15 +105,30 @@ export default class Form extends React.Component<IMyComponentProps, IMyComponen
               <div>
                 <label htmlFor="email" >Qual é o seu e-mail?</label>
               </div>
-              <input type="email" aria-invalid="false" id="email" name="email" placeholder="Insira seu e-mail." value={this.state.email} onChange={change} />s
-          </div>
+              <input type="email"
+                aria-invalid="false"
+                id="email" name="email"
+                placeholder="Insira seu e-mail."
+                value={this.state.email}
+                onChange={change}
+              />
+            </div>
 
             {/* ------ SENHA ---------- */}
             <div>
               <div>
                 <label htmlFor="password">Crie uma senha</label>
               </div>
-              <input type="password" aria-invalid="false" autoComplete="new-password" id="password" pattern=".{8,}" name="password" placeholder="Crie uma senha." value={this.state.password} onChange={change} />
+              <input type="password"
+                aria-invalid="false"
+                autoComplete="new-password"
+                id="password"
+                pattern=".{8,}"
+                name="password"
+                placeholder="Crie uma senha."
+                value={this.state.password}
+                onChange={change}
+              />
             </div>
 
 
@@ -111,7 +136,15 @@ export default class Form extends React.Component<IMyComponentProps, IMyComponen
             <div>
               <div>
                 <label htmlFor="displayname">Como devemos chamar você?</label>
-              </div><input type="text" aria-invalid="false" id="displayname" name="displayname" placeholder="Insira um nome de perfil" value={this.state.displayname} onChange={change} />
+              </div>
+              <input type="text"
+                aria-invalid="false"
+                id="displayname"
+                name="displayname"
+                placeholder="Insira um nome de perfil"
+                value={this.state.displayname}
+                onChange={change}
+              />
               <div>Isso será exibido no seu perfil.</div>
             </div>
 
@@ -126,29 +159,52 @@ export default class Form extends React.Component<IMyComponentProps, IMyComponen
                     <div>
                       <label htmlFor="day">Dia</label>
                     </div>
-                    <input type="text" aria-invalid="false" id="day" inputMode="numeric" maxLength={2} name="day" pattern="((0?[1-9])|([12][0-9])|(3[01]))" placeholder="DD" value={this.state.day} onChange={change} />
+                    <input type="text"
+                      aria-invalid="false"
+                      id="day"
+                      inputMode="numeric"
+                      maxLength={2}
+                      name="day"
+                      pattern="((0?[1-9])|([12][0-9])|(3[01]))"
+                      placeholder="DD"
+                      value={this.state.day}
+                      onChange={change}
+                    />
                   </div>
                 </div>
                 <div data-testid="month"><div>
                   <div>
                     <label htmlFor="month">Mês</label>
-                  </div><div><select id="month" name="month" aria-invalid="false" value={this.state.month} onChange={change}>
-                    <option value="">Mês</option> {/* selected={} disabled={} */}
-                    <option value="01">janeiro</option>
-                    <option value="02">fevereiro</option>
-                    <option value="03">março</option>
-                    <option value="04">abril</option>
-                    <option value="05">maio</option>
-                    <option value="06">junho</option>
-                    <option value="07">julho</option>
-                    <option value="08">agosto</option>
-                    <option value="09">setembro</option>
-                    <option value="10">outubro</option>
-                    <option value="11">novembro</option>
-                    <option value="12">dezembro</option>
-                  </select>
-                    <svg role="img" focusable="false" height="16" width="16" viewBox="0 0 24 24" aria-hidden="true">
-                      <polyline points="20 8 12 17 4 8" fill="none" stroke="#181818"></polyline>
+                  </div>
+                  <div>
+                    <select id="month"
+                      name="month"
+                      aria-invalid="false"
+                      value={this.state.month}
+                      onChange={change}
+                    >
+                      <option value="">Mês</option> {/* selected={} disabled={} */}
+                      <option value="01">janeiro</option>
+                      <option value="02">fevereiro</option>
+                      <option value="03">março</option>
+                      <option value="04">abril</option>
+                      <option value="05">maio</option>
+                      <option value="06">junho</option>
+                      <option value="07">julho</option>
+                      <option value="08">agosto</option>
+                      <option value="09">setembro</option>
+                      <option value="10">outubro</option>
+                      <option value="11">novembro</option>
+                      <option value="12">dezembro</option>
+                    </select>
+                    <svg role="img"
+                      focusable="false"
+                      height="16"
+                      width="16"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <polyline points="20 8 12 17 4 8" fill="none" stroke="#181818" />
                     </svg>
                   </div>
                 </div>
@@ -157,7 +213,17 @@ export default class Form extends React.Component<IMyComponentProps, IMyComponen
                   <div>
                     <label htmlFor="year">Ano</label>
                   </div>
-                  <input type="text" aria-invalid="false" id="year" inputMode="numeric" maxLength={4} name="year" pattern="(19[0-9]{2})|(200)[0-5]" placeholder="AAAA" value={this.state.year} onChange={change} />
+                  <input type="text"
+                    aria-invalid="false"
+                    id="year"
+                    inputMode="numeric"
+                    maxLength={4}
+                    name="year"
+                    pattern="(19[0-9]{2})|(200)[0-5]"
+                    placeholder="AAAA"
+                    value={this.state.year}
+                    onChange={change}
+                  />
                 </div>
                 </div>
               </div>
@@ -169,20 +235,38 @@ export default class Form extends React.Component<IMyComponentProps, IMyComponen
               <legend>Qual é o seu gênero?</legend>
               <div>
                 <div>
-                  <input type="radio" id="gender_option_male" name="gender" value={this.state.gender} aria-invalid="false" onChange={change} />
+                  <input type="radio"
+                    id="gender_option_male"
+                    name="gender"
+                    value={this.state.gender}
+                    aria-invalid="false"
+                    onChange={change}
+                  />
                   <label htmlFor="gender_option_male">
                     <span></span>
                     <span>Masculino</span>
                   </label>
                   <div>
-                    <input type="radio" id="gender_option_female" name="gender" value={this.state.gender} aria-invalid="false" onChange={change} />
+                    <input type="radio"
+                      id="gender_option_female"
+                      name="gender"
+                      value={this.state.gender}
+                      aria-invalid="false"
+                      onChange={change}
+                    />
                     <label htmlFor="gender_option_female">
                       <span></span>
                       <span>Feminino</span>
                     </label>
                   </div>
                   <div>
-                    <input type="radio" id="gender_option_nonbinary" name="gender" value={this.state.gender} aria-invalid="false" onChange={change} />
+                    <input type="radio"
+                      id="gender_option_nonbinary"
+                      name="gender"
+                      value={this.state.gender}
+                      aria-invalid="false"
+                      onChange={change}
+                    />
                     <label htmlFor="gender_option_nonbinary">
                       <span></span>
                       <span>Não binário</span>
